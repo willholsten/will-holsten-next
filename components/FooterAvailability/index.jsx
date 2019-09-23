24fd4914dd2f8c5ea5d0a7f92366203a2778dfe1
Will Holsten <willholsten@date-mike.irisnation.com>
2019-09-23 13:37:08 +0100

    Update work availability

--- components/FooterAvailability/index.jsx
+++ components/FooterAvailability/index.jsx
@@ -6,8 +6,7 @@ export default class FooterAvailability extends React.Component {
     return (
       <span className={style.availability}>
         <span className={style.pulse} />
-        Not currently available for contract positions.
-        <span className={style.date}>(Available early September)</span>
+        Available for contract positions in early November
       </span>
     );
   }
